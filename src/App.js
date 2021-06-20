f5158dd221d3787c45e59a0dfdaf7f760efdd00b
Ankur Kashyap <42894559+KashyapAnkur@users.noreply.github.com>
2021-06-20 23:03:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import UserList from './Components/UserList';
 import UserForm from './Components/UserForm';
 
-
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
           <Switch>
-              
               <Route exact strict path="/" >
                 <Users />
               </Route>
@@ -21,11 +19,9 @@ function App() {
               <Route exact strict path="/userlist">
                 <UserList />
               </Route>
-
           </Switch>
       </BrowserRouter>
     </div>
   );
 }
-
 export default App;
